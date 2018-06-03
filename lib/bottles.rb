cb37760196013533c6014609e488dfc14ea9922e
Douglas Soares de Andrade <contato@douglasandrade.com>
2018-06-03 10:34:51 -0400

    Small changes before moving forward with chapter 1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,34 +4,29 @@ class Bottles
   end
 
   def verse(number_bottles)
-    sing(number_bottles)
+    if number_bottles > 0
+      "#{bottle_or_bottles(number_bottles)} of beer on the wall, #{bottle_or_bottles(number_bottles)} of beer.\n" \
+      "Take #{one_or_it(number_bottles)} down and pass it around, #{bottle_or_bottles(number_bottles-1)} " \
+      "of beer on the wall.\n"
+    else
+      "No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    end
   end
 
   def verses(from, to)
-    from.downto(to).map { |verse| sing(verse) }.join("\n")
+    from.downto(to).map { |verse_number| verse(verse_number) }.join("\n")
   end
 
-  def sing(qty)
-    if qty > 2
-      <<~VERSE
-      #{qty} bottles of beer on the wall, #{qty} bottles of beer.
-      Take one down and pass it around, #{qty-1} bottles of beer on the wall.
-      VERSE
-    elsif qty == 2
-      <<~VERSE
-      #{qty} bottles of beer on the wall, #{qty} bottles of beer.
-      Take one down and pass it around, 1 bottle of beer on the wall.
-      VERSE
-    elsif qty == 1
-      <<~VERSE
-      #{qty} bottle of beer on the wall, #{qty} bottle of beer.
-      Take it down and pass it around, no more bottles of beer on the wall.
-      VERSE
-    else
-      <<~VERSE
-      No more bottles of beer on the wall, no more bottles of beer.
-      Go to the store and buy some more, 99 bottles of beer on the wall.
-      VERSE
-    end
+  private
+
+  def bottle_or_bottles(number_bottles)
+    return "no more bottles" if number_bottles == 0
+    return "1 bottle" if number_bottles == 1
+    "#{number_bottles} bottles"
+  end
+
+  def one_or_it(number_bottles)
+    number_bottles > 1 ? "one" : "it"
   end
 end
